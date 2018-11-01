5ad8d6cb5f4b8e311dfe52c8c3931d5af54518f8
Student <student@nu-student.local>
2018-10-31 19:51:44 -0700

    Add project files.

---
+++ Project1/mainfunc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	using std::cout;
+	using std::endl;
+
+	cout << " mary was here " << endl;
+	cout << "she had lost her lambs";
+
+
+// print out some message 
+	// these are great for notes or comments to help others understand. 
+	return 0;
+}
